f80d0cd0b1e7ff2468de584345113c714902fd80
Vadim Parpolita <vadim.parpolita10@gmail.com>
2024-12-11 09:41:43 +0200

    Removed change detection strategy on push in tooltip component

--- src/app/shared/components/tooltip/tooltip.component.ts
+++ src/app/shared/components/tooltip/tooltip.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { TooltipStyle } from '../../enums/tooltip.enum';
 
@@ -7,8 +7,7 @@ import { TooltipStyle } from '../../enums/tooltip.enum';
   templateUrl: './tooltip.component.html',
   styleUrls: ['./tooltip.component.scss'],
   imports: [NgClass],
-  standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  standalone: true
 })
 export class TooltipComponent {
   style: TooltipStyle = TooltipStyle.Default;
