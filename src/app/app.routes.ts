a9fa092e82ca6dda696a1ab7b592d9e992f9e90f
Vadim Parpolita <vadim.parpolita10@gmail.com>
2025-02-20 10:55:15 +0200

    Deploy Test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
       {
         path: 'main',
         title: 'Dashboard',
-        data: { title: 'Dashboard5' },
+        data: { title: 'Dashboard6' },
         loadChildren: () =>
           import('./features/dashboard/dashboard.module').then(
             (m) => m.DashboardModule
